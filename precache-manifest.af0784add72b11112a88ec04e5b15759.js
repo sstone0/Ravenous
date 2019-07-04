5e6d99a1a70e3387b57ab8ec72fd940b5b106c95
sstone0 <shawnrstone1995@gmail.com>
2019-07-04 10:57:31 -0400

    Updates

---
+++ precache-manifest.af0784add72b11112a88ec04e5b15759.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d5c2198213422628b16625ffaa0f5c",
+    "url": "/Ravenous/index.html"
+  },
+  {
+    "revision": "cb9b99785a115b69bf9a",
+    "url": "/Ravenous/static/css/main.5d470038.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/Ravenous/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "cb9b99785a115b69bf9a",
+    "url": "/Ravenous/static/js/main.8a644b57.chunk.js"
+  },
+  {
+    "revision": "5ddfa0b767d8254e6289",
+    "url": "/Ravenous/static/js/runtime~main.9078551a.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/Ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/Ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Ravenous/precache-manifest.35e4846e9514f0a51e81c7c2aa60f32a.js"
+  "/Ravenous/precache-manifest.af0784add72b11112a88ec04e5b15759.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Ravenous/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ravenous/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
